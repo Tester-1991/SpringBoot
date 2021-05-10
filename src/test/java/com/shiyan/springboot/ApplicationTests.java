8784829ee494768b9d2d499b3d64a046ac52cacd
shiyan <shiyan@newlink.com>
2021-05-10 20:15:04 +0800

    update

--- src/test/java/com/shiyan/springboot/ApplicationTests.java
+++ src/test/java/com/shiyan/springboot/ApplicationTests.java
@@ -1,13 +1,24 @@
 package com.shiyan.springboot;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 
+@Slf4j
 @SpringBootTest
 class ApplicationTests {
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
     @Test
     void contextLoads() {
+
+        long count = jdbcTemplate.queryForObject("select count(*) from user", Long.class);
+
+        log.info("记录总数:{}", count);
     }
 
 }
