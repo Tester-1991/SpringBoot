3da165741fa6a7ccd45f14b162c3fe7ee5c81322
1002450926a <1002450926@qq.com>
2021-05-13 22:25:45 +0800

    自定义DruidConfig，集成Druid监控

--- src/main/java/com/shiyan/springboot/config/DruidConfig.java
+++ src/main/java/com/shiyan/springboot/config/DruidConfig.java
@@ -2,12 +2,15 @@ package com.shiyan.springboot.config;
 
 import com.alibaba.druid.pool.DruidDataSource;
 import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
+import java.util.Arrays;
 
 @Configuration
 public class DruidConfig {
@@ -28,6 +31,25 @@ public class DruidConfig {
     public ServletRegistrationBean statViewServlet() {
         StatViewServlet statViewServlet = new StatViewServlet();
         ServletRegistrationBean<StatViewServlet> servletServletRegistrationBean = new ServletRegistrationBean<>(statViewServlet, "/druid/*");
+        servletServletRegistrationBean.addInitParameter("loginUsername", "admin");
+        servletServletRegistrationBean.addInitParameter("loginPassword", "admin");
         return servletServletRegistrationBean;
+    }
+
+    /**
+     * WebStatFilter用于采集web-jdbc关联监控的数据
+     */
+    @Bean
+    public FilterRegistrationBean webStatFilter() {
+
+        WebStatFilter webStatFilter = new WebStatFilter();
+
+        FilterRegistrationBean<WebStatFilter> filterRegistrationBean = new FilterRegistrationBean(webStatFilter);
+
+        filterRegistrationBean.setUrlPatterns(Arrays.asList("/*"));
+
+        filterRegistrationBean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*");
+
+        return filterRegistrationBean;
     }
 }
